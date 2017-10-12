917e6ff4843ba5d2fa8e11758e3716deb06f51cf
Gregor MacDougall <gregor@freerunningtechnologies.com>
2017-10-12 14:13:48 -0400

    Refactor solver to handle more types of sovling

--- solver.rb
+++ solver.rb
@@ -4,25 +4,37 @@ class Solver
   end
 
   def solveable?
-    solveable = true
-    @sudoku.puzzle.each_with_index do |n, idx|
-      col = idx % 9
-      row = idx / 9
+    previous_iteration = nil
+    iteration = @sudoku
+    while (previous_iteration.nil? || iteration.score != previous_iteration.score) do
+      previous_iteration = iteration
+      iteration.puzzle.each_with_index do |n, idx|
+        col = idx % 9
+        row = idx / 9
 
-      if (n == ' ')
-        solveable = false unless single_candidate?(row, col)
+        if (single_candidate(row, col))
+          new_puzzle = iteration.puzzle.dup
+          new_puzzle[idx] = single_candidate(row, col)
+          iteration = Sudoku.new(new_puzzle)
+        end
       end
     end
-    solveable
+    iteration.score === 0
   end
 
   private
 
-  def single_candidate?(row, col)
-    (
+  def single_candidate(row, col)
+    candidates = (
       @sudoku.rows[row] +
       @sudoku.columns[col] +
       @sudoku.boxes[@sudoku.box_index(row, col)]
-    ).uniq.length === 9
+    ).uniq
+
+    if candidates.length === 9
+      ((1..9).to_a - candidates).first
+    else
+      nil
+    end
   end
 end
